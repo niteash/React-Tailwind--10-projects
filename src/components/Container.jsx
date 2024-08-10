6a0d439a52b03d024767695af8c473041fa9f508
shwewinaung <nickytwist70@gmail.com>
2024-08-10 17:12:59 +0530

    added responsive

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Container = ({children, className}) => {
   return (
-    <div className={`w-full md:w-[800px] lg:w-[1470px] overscroll-x-none overflow-hidden mx-auto  ${className}`}>{children}</div>
+    <div className={`w-full md:w-[1020px]  lg:w-[1470px] overscroll-x-none overflow-hidden mx-auto  ${className}`}>{children}</div>
   )
 }
