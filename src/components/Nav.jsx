9cacd75a23fdf4f06e0a0aeaaf20e3315e981f95
shwewinaung <nickytwist70@gmail.com>
2024-08-10 17:43:09 +0530

    nav bg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,13 +4,13 @@ import { Container } from "./Container";
 const Nav = () => {
   return (
  <Container>
-     <section>
-      <nav className=" cursor-pointer bg-white h p-5 flex justify-between items-center ">
-        <div className="logo">
+      <section>
+      <nav className=" cursor-pointer min-w-full flex-col sm:flex-row  bg-white h p-5 flex justify-between items-center ">
+        <div className="logo hidden sm:block">
           <img src="./images/brand_logo.png" alt="logo" />
         </div>
 
-        <ul className="flex transition-all font-bold uppercase">
+        <ul className="flex transition-all max-w-full sm:mx-auto  font-bold uppercase">
           <li href="#" className="mx-4 hover:text-[#3efb5a] transition-all  hover:underline hover:under">
             <a href="#"> Menu</a>{" "}
           </li>
@@ -28,13 +28,16 @@ const Nav = () => {
           </li>
         </ul>
 
-        <button className="bg-black text-white px-4 py-2  uppercase font-bold transition-all hover:bg-[#3efb5a] hover:text-black">
+        <button className="bg-black text-white hidden sm:block px-4 py-2  uppercase font-bold transition-all hover:bg-[#3efb5a] hover:text-black">
           login
         </button>
       </nav>
-    </section>
+    </section> 
+
+
+
  </Container>
-  );
+  )
 };
 
 export default Nav;
